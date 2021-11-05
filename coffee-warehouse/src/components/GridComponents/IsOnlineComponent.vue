e751c2b1201e03800b1523454456af731cbfa252
Petar Todorov <petar.ptodorov@gmail.com>
2021-11-05 11:03:01 +0200

    chore: use only one span elemen in the IsOnline component

--- coffee-warehouse/src/components/GridComponents/IsOnlineComponent.vue
+++ coffee-warehouse/src/components/GridComponents/IsOnlineComponent.vue
@@ -1,27 +1,11 @@
 <template>
   <span
-    v-if="dataItem.is_online === true"
-    class="
-      k-badge
-      k-badge-solid
-      k-badge-success
-      k-badge-md
-      k-badge-rounded
-      k-badge-inline
-    "
-    >Online</span
-  >
-  <span
-    v-if="dataItem.is_online === false"
-    class="
-      k-badge
-      k-badge-solid
-      k-badge-error
-      k-badge-md
-      k-badge-rounded
-      k-badge-inline
-    "
-    >Offline</span
+    :class="[
+      dataItem.is_online === true
+        ? ' k-badge k-badge-solid k-badge-success k-badge-md k-badge-rounded k-badge-inline'
+        : ' k-badge k-badge-solid k-badge-error k-badge-md k-badge-rounded k-badge-inline',
+    ]"
+    >{{ dataItem.is_online === true ? "Online" : "Offline" }}</span
   >
 </template>
 
