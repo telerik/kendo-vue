3ffe186a249a56e614e3feaa3b4cfabc7e041748
Petar Todorov <petar.todorov@progre>
2022-08-25 15:27:00 +0300

    remove the js definitions of Bootstrap

--- examples-standalone/dashboard/src/main.js
+++ examples-standalone/dashboard/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 // Import our custom CSS
 import './scss/styles.scss'
 
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-
 import router from "./index.js";
 
 import App from './App.vue'
