8dcfbce0f42d904eb2c97e5056cd7f14a461725b
filipKovachev <kovachev@progress.com>
2024-07-25 13:49:49 +0300

    docs(splitbutton): add disable left part kb article

---
+++ knowledge-base/examples/splitbutton-disable-left-part/main.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <splitbutton :items="items" :text="'Enabled Button'" />
+      &nbsp;
+    </div>
+  </template>
+  <script>
+  import { SplitButton } from '@progress/kendo-vue-buttons';
+  
+  export default {
+    components: {
+      splitbutton: SplitButton,
+    },
+    data: function () {
+      return {
+        items: ['Item1', 'Item2', 'Item3'],
+      };
+    },
+  };
+  </script>
+  <style>
+  .k-button-group > .k-button:first-child:not(:only-child) {
+    pointer-events: none;
+  }
+  </style>
+  
