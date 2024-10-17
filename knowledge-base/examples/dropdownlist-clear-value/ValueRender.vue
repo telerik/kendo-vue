305c77ea24491d80c3043052e4806c9431f40856
filipKovachev <kovachev@progress.com>
2024-10-17 12:53:04 +0300

    docs(dropdownlist): add clear value KB

---
+++ knowledge-base/examples/dropdownlist-clear-value/ValueRender.vue
@@ -0,0 +1,43 @@
+<template>
+    <span
+      style="
+        position: relative;
+        display: inline-block;
+        width: 100%;
+        margin-left: 10px;
+        transform: translateY(3px);
+      "
+    >
+      <span>{{ value }}</span>
+  
+      <span
+        :style="{
+          position: 'absolute',
+          right: '0',
+          top: '50%',
+          transform: 'translateY(-60%)',
+          color: '#333',
+          cursor: 'pointer',
+          fontSize: '14px',
+          marginRight: '10px',
+        }"
+        @click="resetValue"
+      >
+        X
+      </span>
+    </span>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      value: String,
+    },
+    methods: {
+      resetValue() {
+        this.$emit('reset');
+      },
+    },
+  };
+  </script>
+  
