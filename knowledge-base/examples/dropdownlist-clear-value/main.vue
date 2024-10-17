25d5066c96a9511ae0de7ec05ba5fe4bc3c86d26
filipKovachev <kovachev@progress.com>
2024-10-17 12:53:04 +0300

    docs(dropdownlist): add clear value KB

---
+++ knowledge-base/examples/dropdownlist-clear-value/main.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <p>Type the name of a European country:</p>
+      <dropdownlist
+        :style="{ width: '230px' }"
+        :data-items="countries"
+        :value="selectedCountry"
+        :value-render="valueRender"
+        @change="onChange"
+      >
+        <template v-slot:myTemplate="{ props }">
+          <custom :value="props.value" @reset="resetValue" />
+        </template>
+      </dropdownlist>
+    </div>
+  </template>
+  
+  <script>
+  import countries from './countries';
+  import { DropDownList } from '@progress/kendo-vue-dropdowns';
+  import ValueRender from './ValueRender';
+  
+  export default {
+    components: {
+      dropdownlist: DropDownList,
+      custom: ValueRender,
+    },
+    data() {
+      return {
+        selectedCountry: countries[0],
+        valueRender: 'myTemplate',
+        countries: countries,
+      };
+    },
+    methods: {
+      onChange(event) {
+        this.selectedCountry = event.value;
+      },
+      resetValue() {
+        this.selectedCountry = '';
+      },
+    },
+  };
+  </script>
+  
